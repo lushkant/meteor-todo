2074aa0f3f7b1d0f2a58d02ce3597444d603a075
Krishna Kant <lushkant@gmail.com>
2018-08-26 04:03:55 -0700

    Fix Typo in account-config

--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 // This is important for meteor to render main.html file.
 import './main.html';
 
+import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,5 +1,5 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.ui.config({
-	passwordSignupFields: 'USERNAMES_ONLY',
+	passwordSignupFields: 'USERNAME_ONLY',
 });
