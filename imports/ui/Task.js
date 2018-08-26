a12c3c82e9605b6950fe2d218ef63236a832acf7
Krishna Kant <lushkant@gmail.com>
2018-08-26 06:31:52 -0700

    Add manual subscribe and publish tasks

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 import { Tasks } from '../api/tasks.js';
 
@@ -12,9 +13,16 @@ export default class Task extends Component {
 		Meteor.call('tasks.remove', this.props.task._id);
 	}
 
-	render() {
+	togglePrivate() {
+		Meteor.call('tasks.setPrivate', this.props.task._id, !this.props.task.private);
+	}
 
-		const taskClassName = this.props.task.checked ? 'checked' : '';
+	render() {
+		const taskClassName = classnames({
+			checked: this.props.task.checked,
+			private: this.props.task.private,
+		});
+
 		return (
 			<li className={taskClassName}>
 				<button className="delete" onClick={this.deleteThisTask.bind(this)} >&times;</button>
@@ -25,6 +33,13 @@ export default class Task extends Component {
 				checked={!!this.props.task.checked}
 				onClick={this.toggleChecked.bind(this)}
 				/>
+
+				{this.props.showPrivateButton ? (
+					<button className="toggle-private" onClick={this.togglePrivate.bind(this)} >
+					{this.props.task.private ? 'Private' : 'Public'}
+					</button>
+				) : '' }
+
 				<span className="text">
 				<strong>{this.props.task.username}</strong>:{this.props.task.text}
 				</span>
